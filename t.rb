eb917bebdc50e37981e6d7fd20365270d53f5340
JonLz <jon.lazar@gmail.com>
2012-08-29 10:59:03 -0400

    Finish layout and routes

---
+++ t.rb
@@ -0,0 +1,5 @@
+person1 = {first: 'Smart', last: 'Ass'}
+person2 = {first: 'Dumb', last: 'Asss'}
+person3 = {first: 'Scratchin', last: 'Assss'}
+
+params = {father: person1, mother: person2, child: person3}
